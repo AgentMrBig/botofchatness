2f9b2563331b76f4f9a436d1cd017d2c35b8fd71
Eric Hendrix <agentmrbig@gmail.com>
2021-03-27 23:52:46 +0000

    Can send chatbot to specific url and will parse an articles text saving it to variable for manipulation and learning.

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ from newspaper import Article
 import random
 import string
 import nltk
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+import numpy as np 
+import warnings
+
+warnings.filterwarnings('ignore')
+
+#Get the article
+article = Article('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')
+
+article.download() 
+article.parse()
+article.nlp() 
+corpus = article.text  
+
+print(corpus)
